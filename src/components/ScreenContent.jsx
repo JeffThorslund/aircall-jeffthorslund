0276e7e10c3aa8fb92ab5814b56d07b48e914a47
Jeff Thorslund <jeffrey.thorslund@gmail.com>
2022-11-22 12:40:20 -0500

    implement tab selection

--- src/components/ScreenContent.jsx
+++ src/components/ScreenContent.jsx
@@ -1,32 +1,69 @@
-import styled from "styled-components";
 import { archiveCallById } from "../requests";
 import { separateArchivedCalls } from "./_utils/separateArchivedCalls";
 import { CallsDisplay } from "./CallsDisplay";
+import { Box, Button } from "grommet";
+import { useState } from "react";
 
-const ContentWrapper = styled.div`
-  padding: 20px;
-`;
+const TabToggle = ({ currentScreenView, setCurrentScreenView }) => {
+  return (
+    <Box direction={"row"} justify={"around"}>
+      <Button
+        primary={currentScreenView === CALL_STATUS.IS_NOT_ARCHIVED}
+        label={"Activity"}
+        onClick={() => setCurrentScreenView(CALL_STATUS.IS_NOT_ARCHIVED)}
+      />
+      <Button
+        primary={currentScreenView === CALL_STATUS.IS_ARCHIVED}
+        label={"Archived"}
+        onClick={() => setCurrentScreenView(CALL_STATUS.IS_ARCHIVED)}
+      />
+    </Box>
+  );
+};
+
+const CALL_STATUS = {
+  IS_ARCHIVED: "isArchived",
+  IS_NOT_ARCHIVED: "isNotArchived",
+};
 
 export const ScreenContent = ({ calls, setCalls }) => {
   const { activityCalls, archivedCalls } = separateArchivedCalls(calls);
+  const [currentScreenView, setCurrentScreenView] = useState(
+    CALL_STATUS.IS_NOT_ARCHIVED
+  );
 
   return (
-    <ContentWrapper>
-      <CallsDisplay
-        title={"Activity"}
-        calls={activityCalls}
-        setCalls={setCalls}
-        onClick={(call) => archiveCallById(call.id, true)}
-        color={"black"}
+    <Box>
+      {(() => {
+        switch (currentScreenView) {
+          case CALL_STATUS.IS_NOT_ARCHIVED:
+            return (
+              <CallsDisplay
+                title={"Activity"}
+                calls={activityCalls}
+                setCalls={setCalls}
+                onClick={(call) => archiveCallById(call.id, true)}
+                color={"black"}
+              />
+            );
+          case CALL_STATUS.IS_ARCHIVED:
+            return (
+              <CallsDisplay
+                title={"Archived"}
+                calls={archivedCalls}
+                setCalls={setCalls}
+                onClick={(call) => archiveCallById(call.id, false)}
+                color={"red"}
+              />
+            );
+          default:
+            <Box>Uh Oh</Box>;
+        }
+      })()}
+      <TabToggle
+        currentScreenView={currentScreenView}
+        setCurrentScreenView={setCurrentScreenView}
       />
-
-      <CallsDisplay
-        title={"Archived"}
-        calls={archivedCalls}
-        setCalls={setCalls}
-        onClick={(call) => archiveCallById(call.id, false)}
-        color={"red"}
-      />
-    </ContentWrapper>
+    </Box>
   );
 };
