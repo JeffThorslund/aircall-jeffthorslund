5f1b039e54df6d0266e253fd6fc697e602768cef
Jeff Thorslund <jeffrey.thorslund@gmail.com>
2022-11-22 10:24:51 -0500

    add test cases for single and mixed items

--- src/components/_utils/separateArchivedCalls.test.js
+++ src/components/_utils/separateArchivedCalls.test.js
@@ -1,8 +1,38 @@
 import { separateArchivedCalls } from "./separateArchivedCalls";
 
-test("blank", () => {
+test("no calls", () => {
   expect(separateArchivedCalls([])).toEqual({
     archivedCalls: [],
     activityCalls: [],
+  });
+});
+
+const archivedCall = { is_archived: true };
+const unarchivedCall = { is_archived: false };
+
+test("single archived call", () => {
+  const data = [archivedCall];
+
+  expect(separateArchivedCalls(data)).toEqual({
+    archivedCalls: [archivedCall],
+    activityCalls: [],
+  });
+});
+
+test("single unarchived call", () => {
+  const data = [unarchivedCall];
+
+  expect(separateArchivedCalls(data)).toEqual({
+    archivedCalls: [],
+    activityCalls: [unarchivedCall],
+  });
+});
+
+test("mixed call types", () => {
+  const data = [archivedCall, unarchivedCall, archivedCall];
+
+  expect(separateArchivedCalls(data)).toEqual({
+    archivedCalls: [archivedCall, archivedCall],
+    activityCalls: [unarchivedCall],
   });
 });
