323ab0d06a9944f1753fc6b743158177bae2d6b4
Jeff Thorslund <jeffrey.thorslund@gmail.com>
2022-11-23 07:15:14 -0500

    add a 'no results' indicator

--- src/components/CallsDisplay.jsx
+++ src/components/CallsDisplay.jsx
@@ -1,25 +1,38 @@
 import { CallTile } from "./CallTile";
 import { replaceCallWithUpdatedCall } from "../_utils/replaceCallWithUpdatedCall";
 import { PageHeader } from "./PageHeader";
+import { Box } from "grommet";
 
 export const CallsDisplay = ({ title, calls, onClick, color, setCalls }) => {
   return (
     <div>
       <PageHeader label={title} />
       <div>
-        {calls.map((c) => (
-          <CallTile
-            call={c}
-            color={color}
-            onClick={() =>
-              onClick(c).then((newCall) =>
-                replaceCallWithUpdatedCall(setCalls, newCall)
-              )
-            }
-            key={c.id}
-          />
-        ))}
+        {calls.length ? (
+          calls.map((c) => (
+            <CallTile
+              call={c}
+              color={color}
+              onClick={() =>
+                onClick(c).then((newCall) =>
+                  replaceCallWithUpdatedCall(setCalls, newCall)
+                )
+              }
+              key={c.id}
+            />
+          ))
+        ) : (
+          <NoResultsToDisplay />
+        )}
       </div>
     </div>
+  );
+};
+
+export const NoResultsToDisplay = () => {
+  return (
+    <Box direction={"row"} justify={"center"} pad={"small"}>
+      There are no calls in this list.
+    </Box>
   );
 };
